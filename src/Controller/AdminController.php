754a01e243aa6beed64fc70a14431ee7fdc5a4cc
Diogo Gomes <contact@alphawebmaker.com>
2020-08-12 13:50:29 +0200

    Feature/layouts (#22)
    * Integrate Admin Controller
    
    * Integrate Admin template
    
    Co-authored-by: Alphabetus <d.gomes@whic.de>

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function hq()
+    {
+        return $this->render("base_admin.html.twig");
+    }
+}

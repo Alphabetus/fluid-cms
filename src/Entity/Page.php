3394eb5dbcc529c1a2a188ab97e4962b0e5f43ed
Alphabetus <d.gomes@whic.de>
2020-08-14 11:44:07 +0200

    Integrate Page Entity

---
+++ src/Entity/Page.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PageRepository::class)
+ */
+class Page
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $in_navigation;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $active;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $priority_list;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getInNavigation(): ?bool
+    {
+        return $this->in_navigation;
+    }
+
+    public function setInNavigation(?bool $in_navigation): self
+    {
+        $this->in_navigation = $in_navigation;
+
+        return $this;
+    }
+
+    public function getActive(): ?bool
+    {
+        return $this->active;
+    }
+
+    public function setActive(?bool $active): self
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    public function getPriorityList(): ?string
+    {
+        return $this->priority_list;
+    }
+
+    public function setPriorityList(?string $priority_list): self
+    {
+        $this->priority_list = $priority_list;
+
+        return $this;
+    }
+}

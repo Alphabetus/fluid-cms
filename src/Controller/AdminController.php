066a13ca604903eae8dbdc07361750d3eb726004
Alphabetus <d.gomes@whic.de>
2020-08-17 11:59:12 +0200

    Integrate locales with example

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,15 +6,18 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class AdminController extends AbstractController
 {
     /**
      * @Route("/admin", name="admin")
      */
-    public function hq()
+    public function hq(TranslatorInterface $translator)
     {
-        return $this->render("base_admin.html.twig");
+        return $this->render("base_admin.html.twig", [
+            "translator" => $translator
+        ]);
     }
 
     /**

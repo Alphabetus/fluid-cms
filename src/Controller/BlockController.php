cc3e151fbe704e0699839c90c47788e6dba6c5b2
Alphabetus <d.gomes@whic.de>
2020-08-18 14:30:02 +0200

    init blocks behaviour

--- src/Controller/BlockController.php
+++ src/Controller/BlockController.php
@@ -4,12 +4,16 @@
 namespace App\Controller;
 
 
+use App\Entity\Block;
+use App\Entity\Page;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Uid\Uuid;
 
-class BlockController
+class BlockController extends AbstractController
 {
     /**
      * @param Request $request
@@ -18,6 +22,43 @@ class BlockController
      */
     public function createBlock(Request $request): JsonResponse
     {
-        return new JsonResponse("WORKS", 200);
+        $id = $request->request->get("page_id");
+        $type =  $request->request->get("type");
+        $page = $this->getDoctrine()->getRepository(Page::class)->find($id);
+        $block = new Block();
+        $em = $this->getDoctrine()->getManager();
+        $block->setPage($page);
+        $block->setType($type);
+        $block->setMobileBreakpoint("col-12");
+        $block->setDesktopBreakpoint("col-md-12");
+        $block->setBuid(Uuid::v1());
+        $em->persist($block);
+        $em->flush();
+
+
+        return new JsonResponse($block->getBuid(), 200);
+    }
+
+    /**
+     * @param Request $request
+     * @Route("/blocks/delete", name="blocks.delete")
+     */
+    public function removeBlock(Request $request): JsonResponse
+    {
+        $buid = $request->request->get("buid");
+        $block = $this->getDoctrine()->getRepository(Block::class)->findOneBy(["buid" => $buid]);
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($block);
+        $em->flush();
+        return new JsonResponse("ok", 200);
+    }
+
+    protected function logger($content)
+    {
+        $file = "test.php";
+        ob_start();
+        var_dump($content);
+        $testing = ob_get_clean();
+        file_put_contents($file, $testing, FILE_APPEND);
     }
 }

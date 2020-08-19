67c4808688535927c51340485ca4c666ac1ddf0b
Alphabetus <d.gomes@whic.de>
2020-08-19 15:14:40 +0200

    add 404 page

---
+++ src/Controller/BlogfrontController.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\Page;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogfrontController extends AbstractController
+{
+    /**
+     * @param string $slug
+     * @return Response
+     * @Route("/{slug}", name="blogfront.page")
+     */
+    public function showPage(string $slug): Response
+    {
+        $page = $this->getDoctrine()->getRepository(Page::class)->findOneBy(["slug" => $slug]);
+        if (!$page) {
+            return $this->redirectToRoute("blogfront.not_found");
+        }
+        return $this->render("blogfront/show_page.html.twig", [
+            "page" => $page
+        ]);
+    }
+
+    /**
+     * @return Response
+     * @Route("/page/not-found", name="blogfront.not_found")
+     */
+    public function notFound(): Response
+    {
+        return $this->render("blogfront/404.html.twig");
+    }
+}

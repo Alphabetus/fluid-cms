0044ce109c8e6d46893522f6ddadd50c2991c885
Alphabetus <d.gomes@whic.de>
2020-08-21 16:15:53 +0200

    revert to valid locale

--- src/Controller/SettingController.php
+++ src/Controller/SettingController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class SettingController extends AbstractController
 {
@@ -48,9 +49,17 @@ class SettingController extends AbstractController
      *     defaults={"_locale"="en"},
      *     options={"expose"=true}
      * )
+     * @param Request $request
+     * @param TranslatorInterface $translator
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
      */
-    public function showSettings()
+    public function showSettings(Request $request, TranslatorInterface $translator)
     {
+        $locale = $request->getLocale();
+        if (!in_array($locale, AdminController::getValidLocales())){
+            $this->addFlash("error", $translator->trans("app.controller.admincontroller.locale_not_found"));
+            return $this->redirectToRoute("admin", ["_locale" => "en"]);
+        }
         $pages = $this->getDoctrine()->getRepository(Page::class)->findAll();
         $current_homepage = $this->getDoctrine()->getRepository(GlobalSetting::class)->findOneBy(["name" => "homepage"])->getValue();
         $current_maintenance = $this->getDoctrine()->getRepository(GlobalSetting::class)->findOneBy(["name" => "maintenance"])->getValue();
